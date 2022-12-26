d235b735c6281819c1a02470d232b0c0b4f9af40
Bemart93 <bemart93@gmail.com>
2022-12-26 19:13:49 +0100

    Iteracion#7

---
+++ ejercicio-7-forof-avanzado.js
@@ -0,0 +1,22 @@
+//Usa un bucle for...of para recorrer todos los juguetes y añade los que tengan más de 15 ventas (sellCount) al array popularToys. Imprimelo por consola.. Puedes usar este array:
+
+const popularToys = [];
+const toys = [
+	{id: 5, name: 'Buzz MyYear', sellCount: 10}, 
+	{id: 11, name: 'Action Woman', sellCount: 24}, 
+	{id: 23, name: 'Barbie Man', sellCount: 15}, 
+	{id: 40, name: 'El gato con Guantes', sellCount: 8},
+	{id: 40, name: 'El gato felix', sellCount: 35}
+]
+
+for (const toy of toys) {
+    
+    const HighSellCount = toy.sellCount >= 15;
+
+    console.log(HighSellCount)
+
+    if( HighSellCount){
+        popularToys.push(toy)
+    } 
+    
+}console.log(popularToys)
